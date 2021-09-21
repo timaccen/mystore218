fe535727807918f92dda988b5909b0dd344abd37
timothy.curchod <timothy.curchod@accenture.com>
2021-09-21 16:14:19 +1000

    #2 added the layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { translations, translationChunksConfig } from '@spartacus/assets';
 import { B2cStorefrontModule } from '@spartacus/storefront';
+import { LayoutConfigurationModule } from './configuration/layout.module';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { B2cStorefrontModule } from '@spartacus/storefront';
       features: {
         level: '2.1'
       }
-    })
+    }),
+    LayoutConfigurationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
